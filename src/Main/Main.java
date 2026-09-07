bda928fa9f150155a967b98c45fa30d4456ef86d
Eriri1111 <alfonsodaraujo@unicesar.edu.co>
2026-09-06 21:09:14 -0500

    creacion de todas las carpetas del proyecto

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,18 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package taller.pkg2.gamezoneunicesar;
+package Main;
 
 /**
  *
  * @author Familia
  */
-public class Taller2GameZoneUnicesar {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
     }
     

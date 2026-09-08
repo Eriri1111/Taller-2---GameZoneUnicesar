c9d88834ca8c62a78df208b4273ae03f87be3ca2
Sebastian Andres Bornachera <sebastianalopez@unicesar.edu.co>
2026-09-07 23:16:25 -0500

    feat: se crearon los metodos setter de la clase console

---
+++ src/Main/Dao/ProductRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main.Dao;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProductRepository {
+    private final String filePath;
+    
+    public ProductRepository (String filePath){
+        this.filePath = filePath;
+    }
+
+    
+    
+    
+}

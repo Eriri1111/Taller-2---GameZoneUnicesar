f61ddc609f34bd693ffe8bca21551e4e147152b8
Sebastian Andres Bornachera <sebastianalopez@unicesar.edu.co>
2026-09-07 23:43:20 -0500

    feat: se creo y desarrollo el metodo loadProducts de la clase productRepository

--- src/Main/Dao/ProductRepository.java
+++ src/Main/Dao/ProductRepository.java
@@ -7,9 +7,13 @@ package Main.Dao;
 import Main.Product;
 import Main.VideoGame;
 import Main.Console;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -45,7 +49,47 @@ public class ProductRepository {
             System.err.println("Error saving products to file: " + e.getMessage());
         }
     }
-
     
+      public List<Product> loadProducts() {
+        List<Product> products = new ArrayList<>();
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            return products;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(";");
+                if (data.length < 8) continue;
+
+                String type = data[0];
+                String id = data[1];
+                String title = data[2];
+                double price = Double.parseDouble(data[3].replace(",", "."));
+                int stock = Integer.parseInt(data[4]);
+
+                if ("GAME".equals(type)) {
+                    String platform = data[5];
+                    String genre = data[6];
+                    String ageRating = data[7];
+                    products.add(new VideoGame(id, title, price, stock, platform, genre, ageRating));
+                } else if ("CONSOLE".equals(type)) {
+
+                    String brand = data[5];
+                    String model = data[6];
+                    String generation = data[7];
+
+                    products.add(
+                        new Console(id, title,price, stock, brand,  model,generation));
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.err.println("Error loading products from file: " + e.getMessage());
+        }
+
+        return products;
+    }     
     
 }

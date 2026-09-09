7e91fd3a249d490852818e8dd842a7c6f3c07914
Sebastian Andres Bornachera <sebastianalopez@unicesar.edu.co>
2026-09-08 23:20:15 -0500

    feat: se creo la clase promotion con sus atributos y constructor

---
+++ src/Model/Promotion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Promotion {
+    private String id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    protected Promotion(String id, String name, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
